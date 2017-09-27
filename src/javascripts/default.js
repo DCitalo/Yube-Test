4ab8523164bb394adc97656d9e9e34ff2246c607
DCitalo <cd_italo@hotmail.com>
2017-09-27 02:13:08 -0300

    Test 1.0.0

--- src/javascripts/default.js
+++ src/javascripts/default.js
@@ -2,5 +2,6 @@ var app = angular.module('YubeApp', []);
 app.controller('myCtrl', function($scope, $http) {
     $http.get("https://jsonplaceholder.typicode.com/posts")
     .then(function(response) {
-    	var i;
-    	for (i = 0; i < response.data.length; 
+        $scope.Data = response.data;
+    });
+})
